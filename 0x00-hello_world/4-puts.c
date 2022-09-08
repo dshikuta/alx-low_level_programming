5443a11ec735f53278b21fbcefbdbba39360bb31
dshikuta <dshikuta@gmail.com>
2022-09-08 10:09:34 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
-
-
 /**
  *
- *  * main - Entry point
+ * * main - Prints using puts() command
  *
- *   *
+ * * Return: 0 prints something
  *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * */
 
 int main(void)
 
