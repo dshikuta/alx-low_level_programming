8cfe8c84436ca476f793bdd3358fe2b6975a3142
dshikuta <dshikuta@gmail.com>
2022-09-11 13:35:36 -0700

    print combs

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,13 +17,13 @@ int main(void)
 
 
 			for (d = 0; d < 100; d++)
+	{
+			putchar((d / 10) + '0');
 
-					{
-						putchar((d / 10) + '0');
+			putchar((d % 10) + '0');
 
-						putchar((d % 10) + '0');
-							if (d != 99)
-			{
+			if (d != 99)
+	{
 																			putchar(',');
 																			putchar(' ');
 																					}
