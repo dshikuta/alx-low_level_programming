84e40cab44c65bf053281c8f2631786093913aeb
dshikuta <dshikuta@gmail.com>
2022-09-12 05:49:09 -0700

    positive or negative

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,29 +8,19 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-
 		int d;
-
-
-			for (d = 0; d < 100; d++)
+	for (d = 0; d < 100; d++)
+{
+	putchar((d / 10) + '0');
+putchar((d % 10) + '0');		
+	if (d != 99)
 	{
-			putchar((d / 10) + '0');
-
-			putchar((d % 10) + '0');
-
-			if (d != 99)
-	{
-																			putchar(',');
-																			putchar(' ');
-																					}
-					}
-
-				putchar('\n');
-
-					return (0);
-
+	putchar(',')
+	putchar(' ')
+	}	
+}
+	putchar('\n');
+	return (0);
 }
