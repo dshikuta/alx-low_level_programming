cba56ec953a8203c4bbf76f22eb94c472dd622ef
dshikuta <dshikuta@gmail.com>
2022-09-12 09:40:52 -0700

    print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,43 +5,22 @@
 #include <time.h>
 
 /**
- *
- *  * main - Prints the alphabet in lowercase, followed by a new line.
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main - Prints the alphabet in lowercase, followed by a new line.
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	char letter;
 
-		char letter;
-
-
-
-			for (letter = 'a'; letter <= 'z'; ++letter)
-
-					{
-
-								putchar(letter);
-
-									}
-
-				for (letter = 'A'; letter <= 'Z'; ++letter)
-
-						{
-
-									putchar(letter);
-
-										}
-
-					putchar('\n');
-
-
-
-						return (0);
-
+	for (letter = 'a'; letter <= 'z'; ++letter
+			)
+	{
+	putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; ++letter)
+	{
+	putchar(letter);
+	}
+	putchar('\n');
+	return (0);
 }
