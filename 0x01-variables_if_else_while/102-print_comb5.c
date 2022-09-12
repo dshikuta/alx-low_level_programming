e33550ee1123a98cb8f7426bf7e125faf5d51e56
dshikuta <dshikuta@gmail.com>
2022-09-12 16:15:39 -0700

    print comb2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,25 +2,43 @@
 
 #include <stdlib.h>
 
-#include <time.h>
-
 /**
- * main - Entry point
+ * main - A program that prints combination of all two digit numbers 0-99.
  * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
-		int d;
-	for (d = 0; d < 100; d++)
-{
-	putchar((d / 10) + '0');
-putchar((d % 10) + '0');		
-	if (d != 99)
+	int fTens;
+	int fOnes;
+	int sTens;
+	int sOnes;
+	int num, num2;
+
+	for (num = 0; num <= 99; num++)
+
 	{
-	putchar(',')
-	putchar(' ')
-	}	
-}
+	fTens = num / 10;
+	fOnes = num % 10;
+	for (num2 = 0; num2 <= 99; num2++)
+	{
+	sTens = num2 / 10;
+	sOnes = num2 % 10;
+	if (fTens < sTens || (fTens == sTens && fOnes < sOnes))
+	{
+	putchar(fTens + '0');
+	putchar(fOnes + '0');
+	putchar(' ');
+	putchar(sTens + '0');
+	putchar(sOnes + '0');
+	if (!(fTens == 9 && fOnes == 8))
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+																	}
 	putchar('\n');
 	return (0);
 }
