d8d48a89b94e8e6d66c58ab74ed0f592f01729fa
dshikuta <dshikuta@gmail.com>
2022-09-13 07:38:57 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ * main - prints the string "_putchar" from a character array.
+ * Return: 0 on success.
+ */
+
+int main(void)
+
+{
+
+	char c[] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 9; i++)
+
+	{
+	_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
