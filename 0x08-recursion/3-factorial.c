db865b3a0ffffa45d85c490ea25fc0e41b9034d5
dshikuta <dshikuta@gmail.com>
2022-09-28 16:18:55 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * factorial - returns the length of a string
+ * @n: int arg
+ * Return: int
+ */
+
+int factorial(int n)
+
+{
+
+	if (n < 0)
+
+	{
+
+		return (-1);
+
+	}
+
+	if (n > 0)
+
+	{
+
+		return (n * factorial(n - 1));
+
+	}
+
+	return (1);
+}
