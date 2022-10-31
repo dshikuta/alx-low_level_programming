f71658340dfc54e9e786ee2dde59cf9a5a034b67
dshikuta <dshikuta@gmai.com>
2022-10-31 03:13:00 -0700

    header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef "main.h"
-#define "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -16,4 +16,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 
-#endif
+#endif */ MAIN_H */
