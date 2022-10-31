dee015d7505ea86df725a13bd1e56d0617f03bd1
dshikuta <dshikuta@gmai.com>
2022-10-31 03:05:04 -0700

    a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Write a program that prints Holberton, followed by a new*
+ * main - Write a program that prints _putchar followed by a new*
  * Return: On success 0.
  */
 int main(void)
@@ -9,21 +9,13 @@ int main(void)
 {
 
 	_putchar('_');
-
 	_putchar('p');
-
 	_putchar('u');
-
 	_putchar('t');
-
 	_putchar('c');
-
 	_putchar('h');
-
 	_putchar('a');
-
 	_putchar('r');
-
 	_putchar('\n');
 
 	return (0);
