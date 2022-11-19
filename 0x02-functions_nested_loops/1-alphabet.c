af629a2971880cd6f94d924c8cdcd725621a657d
dshikuta <dshikuta@gmai.com>
2022-11-19 07:17:05 -0800

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,22 +2,23 @@
 
 /**
  * print_alphabet - prints 10 times the alphabet
+ *
  * Return: On success 1.
  */
 
 void print_alphabet(void)
 
 {
-
 	int letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 
 	{
 
-	_putchar(letter);
+		_putchar(letter);
 
 	}
-		_putchar('\n');
+
+	_putchar('\n');
 
 }
