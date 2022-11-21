db2f44181009edaa9684dfdfbac92174fddba23f
dshikuta <dshikuta@gmai.com>
2022-11-21 08:31:31 -0800

    more singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,52 +1,50 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes a node in a linked list at a certain index
- * @head: pointer to the first element in the list
- * @index: index of the node to delete
- * Return: 1 (Success), or -1 (Fail)
+ * delete_nodeint_at_index - Deletes the node at a given
+ *                           index of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @index: The index of the node to be deleted - indices start at 0.
+ *
+ * Return: On success - 1.
+ *         On failure - -1.
  */
-
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
-
 {
+	listint_t *tmp, *copy = *head;
 
-	listint_t *temp = *head;
+	unsigned int node;
 
-	listint_t *current = NULL;
+	if (copy == NULL)
 
-	unsigned int i = 0;
-
-	if (*head == NULL)
 		return (-1);
 
 	if (index == 0)
-	{
-											*head = (*head)->next
-												free(temp);
-											return (1);
 
+	{
+		*head = (*head)->next;
+
+		free(copy);
+
+		return (1);
 	}
-
-	while (i < index - 1)
-
+	for (node = 0; node < (index - 1); node++)
 	{
 
-		if (!temp || !(temp->next))
+		if (copy->next == NULL)
 
 			return (-1);
 
-		temp = temp->next;
-
-		i++;
+	copy = copy->next;
 
 	}
 
-	current = temp->next;
+	tmp = copy->next;
 
-	temp->next = current->next;
+	copy->next = tmp->next;
 
-	free(current);
+	free(tmp);
 
 	return (1);
 }
