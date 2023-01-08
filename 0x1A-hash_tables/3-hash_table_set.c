3b4d37a6919c7b76665999c4c9f35bf004576fd0
dshikuta <dshikuta@gmail.com>
2023-01-08 12:20:33 -0800

    a function that adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,84 +1,51 @@
 #include "hash_tables.h"
 
 /**
- *  hash_table_set - Adds an element to the hash table
- *  @ht: The hash table to update
- *  @key: Key to hashtable
- *  @value: Value associated with key
- *  Return: A pointer to the newly created hash table
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int slot = 0;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-	hash_node_t *new_hash = NULL, *temp = NULL;
-
-	new_hash = malloc(sizeof(hash_node_t));
-
-	if (ht == NULL || !key || strlen(key) == 0 || value == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	if (new_hash == NULL)
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
-	temp = new_hash;
-	slot = key_index((const unsigned char *)key, ht->size);
 
-	if (ht->array[slot] != NULL)
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		new_hash = ht->array[slot];
-
-		while (new_hash != NULL)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			if (strcmp(new_hash->key, key) == 0)
-			{
-				free(new_hash->value);
-				new_hash->value = strdup(value);
-				free(temp);
-				return (1);
-			}
-			new_hash = new_hash->next;
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
 		}
 	}
-	new_hash = hash_table_pair(key, value);
-	if (new_hash == NULL)
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
-		free(new_hash);
+
+		free(value_copy);
 		return (0);
 	}
-	new_hash->next = ht->array[slot];
-	ht->array[slot] = new_hash;
-	free(temp);
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 	return (1);
-}
-/**
- *  hash_table_pair - Allocate memory for entry
- *  @key: Key to hashtable
- *  @value: Value associated with key
- *  Return: Entry of key/value
- */
-hash_node_t *hash_table_pair(const char *key, const char *value)
-{
-	hash_node_t *entry = NULL;
-
-	entry = malloc(sizeof(hash_node_t));
-
-	if (entry == NULL)
-	{
-		return (NULL);
-	}
-	entry->key = strdup(key);
-	if (entry->key == NULL)
-	{
-		free(entry);
-		return (NULL);
-	}
-	entry->value = strdup(value);
-	if (entry->value == NULL)
-	{
-		free(entry->key);
-		free(entry);
-		return (NULL);
-	}
-	entry->next = NULL;
-	return (entry);
 }
